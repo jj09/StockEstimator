88cd928c88c01d68dbb8a414758a93e0c3de9465
Jakub Jedryszek <jakub.jedryszek@gmail.com>
2016-01-31 21:06:59 -0800

    Initial commit (estimation working)

---
+++ StockEstimator.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockEstimator.Logic;
+
+namespace StockEstimator.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stockData = new StockData();
+
+            // fetching stock data
+            var msftRows = stockData.getStockData("MSFT");
+            //foreach (var row in msftRows)
+            //{
+            //    Console.WriteLine($"{row.Key}: {string.Format("{0:.00}", float.Parse(row.Value))}");
+            //}
+
+            // fetching stock data for date/time range
+            var msftRowsForLastYear = stockData.getStockDataForDateRange("MSFT", DateTime.Now.AddYears(-1), DateTime.Now);
+            //foreach (var row in msftRowsForLastYear)
+            //{
+            //    Console.WriteLine($"{row.Key}: {string.Format("{0:.00}", float.Parse(row.Value))}");
+            //}
+
+            // estimating future price
+            var futureDate = DateTime.Now.AddDays(5);
+            var estimatedPrice = stockData.getEstimatedPriceForDate("MSFT", futureDate, DateTime.Now.AddMonths(-4));
+            Console.WriteLine($"Estimated price for {futureDate.ToShortDateString()} is {string.Format("{0:.00}", estimatedPrice)}");            
+        }
+    }
+}
---
+++ StockEstimator.Logic/StockData.fs
@@ -0,0 +1,48 @@
+﻿namespace StockEstimator.Logic
+
+open FSharp.Data
+open MathNet
+open System
+
+type Stocks = CsvProvider<"Date,Open,High,Low,Close,Volume,Adj Close">
+
+type StockData() = 
+
+    let getBaseStockUrl ticker =
+        "http://ichart.finance.yahoo.com/table.csv?s=" + ticker
+    
+    let getUrlForDateTimeRange ticker (startDate:DateTime) (endDate:DateTime) = 
+        let root = getBaseStockUrl ticker
+        sprintf "%s&a=%i&b=%i&c=%i&d=%i&e=%i&f=%i" 
+            root (startDate.Month - 1) startDate.Day startDate.Year 
+                        (endDate.Month - 1) endDate.Day endDate.Year
+
+    member this.getStockData ticker =
+        let stockData = Stocks.Load(getBaseStockUrl ticker)
+        dict (stockData.Rows |> Seq.map (fun x -> x.Date, x.Close))
+
+    member this.getStockDataForDateRange ticker (startDate:DateTime) (endDate:DateTime) =
+        let url = getUrlForDateTimeRange ticker startDate endDate
+        let stockData = Stocks.Load(url)
+        dict (stockData.Rows |> Seq.map (fun x -> x.Date, x.Close))
+
+    member this.getEstimatedPriceForDate (ticker, forDate: DateTime, fromDate: DateTime) =
+        //// uncomment below when forDate and fromDate are optional (?forDate, ?fromDate)
+        //let forDate = defaultArg forDate (DateTime.Now.AddDays(1.))
+        //let fromDate = defaultArg fromDate (DateTime.Now.AddYears(-1))
+
+        let stockData = Stocks.Load(getUrlForDateTimeRange ticker fromDate (DateTime.Now)).Rows
+
+        let firstDate = DateTime.Parse((stockData |> Seq.last).Date)
+        
+        // refactor to one-liner
+        let adjData = stockData |> Seq.map (fun x -> ((DateTime.Parse(x.Date) - firstDate).TotalDays, float x.Close)) 
+        let xData = adjData |> Seq.map (fun x -> fst x) |> Seq.toArray
+        let yData = adjData |> Seq.map (fun x -> snd x) |> Seq.toArray
+
+        let forDateInDays = (forDate - firstDate).TotalDays
+
+        let intercept, slope = MathNet.Numerics.Fit.Line(xData, yData)
+        
+        let result = forDateInDays*slope + intercept
+        result
