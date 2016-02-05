c415b7a2c3378e78acc7824866fbcd2f2f851b7a
Jakub Jedryszek <jakub.jedryszek@gmail.com>
2016-02-04 23:19:49 -0800

    Added Tests project

---
+++ StockEstimator.Tests/StockDataTests.fs
@@ -0,0 +1,22 @@
+﻿namespace StockDataEstimator.Tests
+
+open System
+open StockEstimator.Logic
+
+module StockDataTests =
+    open Xunit
+    open System.Linq
+
+    [<Fact>]
+    let ``returns stock data in correct format`` () =
+        // Arrange
+        let stockData = StockData()        
+
+        // Act
+        let data = stockData.GetStockData "msft"
+        let firstElement = Enumerable.ToList(data.Keys).[0]
+        let firstValue = data.Values.First()
+
+        // Assert
+        Assert.Equal(DateTime.Now.GetType(), firstElement.GetType())
+        Assert.Equal((decimal 1).GetType(), firstValue.GetType())
